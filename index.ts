9fe5389e6f203c4e7d85d7778c02a2affe47249e
Adrien Vergnaud <a.vergnaud@gmail.com>
2018-02-24 16:05:54 +0100

    suite

--- index.ts
+++ index.ts
@@ -1,11 +1,54 @@
-
 /**
  * Versement (paiement)
  */
-export default interface Versement {
-    id:number
-    etat:string
-    client:string
-    montant:number
-    commentaire:string
+export default class Versement {
+  id: number;
+  etat: string;
+  client: string;
+  montant: number;
+  commentaire: string;
+
+  constructor(input: any) {
+    this.id = input.id;
+    this.etat = input.etat;
+    this.client = input.client;
+    this.montant = input.montant;
+    this.commentaire = input.commentaire;
+  }
+
+  getId(): number {
+    return this.id;
+  }
+  setId(newId: number) {
+    this.id = newId;
+  }
+
+  getEtat(): string {
+      return this.etat;
+  }
+  setEtat(newEtat: string) {
+      this.etat = newEtat;
+  }
+
+  getClient(): string {
+      return this.client;
+  }
+  setClient(newClient: string) {
+      this.client = newClient;
+  }
+
+  getMontant(): number {
+      return this.montant;
+  }
+  setMontant(newMontant: number) {
+      this.montant = newMontant;
+  }
+
+  getCommentaire(): string {
+      return this.commentaire;
+  }
+  setCommentaire(newCommentaire: string) {
+      this.commentaire = newCommentaire;
+  }
+
 }
